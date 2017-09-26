e4535cbf125145e2f415f5c7e856a1604bc24cf9
Matt Daisley <mattdaisley@gmail.com>
2017-09-26 04:41:41 -0600

    fix prod api endpoint

--- core/client/src/App/Redux/SuperViewApi/SuperViewConfig.js
+++ core/client/src/App/Redux/SuperViewApi/SuperViewConfig.js
@@ -6,8 +6,8 @@ switch (process.env.NODE_ENV) {
     // apiRequestUri = 'https://www.superview.tv/api/v0.1/';
     break;
   case 'production':
-    apiRequestUri = 'http://127.0.0.1:7768/api/v0.1/';
-    // apiRequestUri = 'https://www.superview.tv/api/v0.1/';
+    // apiRequestUri = 'http://127.0.0.1:7768/api/v0.1/';
+    apiRequestUri = 'https://www.superview.tv/api/v0.1/';
     break;
   default:
     break;
