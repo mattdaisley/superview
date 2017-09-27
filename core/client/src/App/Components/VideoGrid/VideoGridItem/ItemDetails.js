e4aa2d98da0ec08f97897669e83509f95519e515
Matt Daisley <mattdaisley@gmail.com>
2017-09-27 01:47:25 -0600

    item hover play and add button styles

--- core/client/src/App/Components/VideoGrid/VideoGridItem/ItemDetails.js
+++ core/client/src/App/Components/VideoGrid/VideoGridItem/ItemDetails.js
@@ -2,17 +2,52 @@ import React     from 'react';
 import PropTypes from 'prop-types';
 import { Link }  from 'react-router-dom';
 
-import Button   from 'material-ui/Button';
+import Button        from 'material-ui/Button';
+import IconButton    from 'material-ui/IconButton';
 import PlayArrowIcon from 'material-ui-icons/PlayArrow';
+import AddIcon       from 'material-ui-icons/Add';
 
 import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
   playButton: {
     position: 'absolute',
-    right: 25,
+    marginLeft: -40,
+    marginBottom: -40,
+    left: '50%',
+    color: 'white',
+    bottom: '60%'
   },
   playButtonOut: {
+    opacity: 0,
+    transition: [
+      theme.transitions.create('opactiy', {
+        easing: theme.transitions.easing.easeIn,
+        duration: theme.transitions.duration.shortest,
+      }),
+    ],
+  },
+  playButtonHover: {
+    opacity: 1,
+    transition: [
+      theme.transitions.create('opacity', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.shortest,
+      }),
+    ],
+  },
+  playHover: {
+    color: '#448aff'
+  },
+  playHoverDim: {
+    opacity: .2
+  },
+  addButton: {
+    position: 'absolute',
+    right: 25,
+    color: 'white'
+  },
+  addButtonOut: {
     bottom: -100,
     transition: [
       theme.transitions.create('bottom', {
@@ -21,7 +56,7 @@ const styles = theme => ({
       }),
     ],
   },
-  playButtonHover: {
+  addButtonHover: {
     bottom: 25,
     transition: [
       theme.transitions.create('bottom', {
@@ -30,7 +65,29 @@ const styles = theme => ({
       }),
     ],
   },
+  addHover: {
+    color: '#448aff'
+  },
 })
+
+const overrideStyles = {
+  playButton: {
+    background: 'rgba(0,0,0,.6)',
+    borderRadius: 50,
+    border: '2px solid white',
+    height: 80,
+    width: 80,
+  },
+  playArrowIcon: {
+    width: 50,
+    height: 50,
+  },
+  addButton: {
+    background: 'rgba(0,0,0,.6)',
+    borderRadius: 50,
+    border: '1px solid white',
+  },
+}
 
 // const ItemImage = (props) => {
 class ItemDetails extends React.PureComponent {
@@ -58,20 +115,38 @@ class ItemDetails extends React.PureComponent {
     // const id          = props.videoItem.id;
     // const channelName = ( props.videoItem.channel ) ? props.videoItem.channel.name : null;
     // const views       = ( props.videoItem.stats ) ? props.videoItem.stats.views.toLocaleString('en-IN') + ' ' + ( props.source === 'tw' ? 'viewers' : 'views' ) : null;
-    
-    const playButtonHoverClass = ( !!this.state.hover ) ? classes.playButtonHover : classes.playButtonOut;
 
+    let playButtonClass = classes.playButton;
+    if ( !!this.state.hover ) { playButtonClass += ' ' + classes.playButtonHover } else { playButtonClass += ' ' + classes.playButtonOut };
+    if ( !!this.state.addHover ) { 
+      playButtonClass += ' ' + classes.playHoverDim 
+    } else {
+      if ( !!this.state.playHover ) { playButtonClass += ' ' + classes.playHover };
+    }
+
+
+    let addButtonClass = classes.addButton;
+    if ( !!this.state.hover ) { addButtonClass += ' ' + classes.addButtonHover } else { addButtonClass += ' ' + classes.addButtonOut };
+    if ( !!this.state.addHover ) { addButtonClass += ' ' + classes.addHover };
+    
     return (
-      <div className="list-grid-details-container" onMouseEnter={ () => this.setState({hover:true})} onMouseLeave={ () => this.setState({hover:false})}>
+      <div className="list-grid-details-container" onMouseEnter={ () => this.setState({hover:true, playHover:true})} onMouseLeave={ () => this.setState({hover:false, playHover:true})}>
         <Link to={route}>
           <div className="list-grid-details">
             <div className="list-grid-details-title">{title}</div>
             <div className="list-grid-details-channel">{channelName}</div>
             <div className="list-grid-details-info">{views}</div>
           </div>
+
+          <IconButton style={overrideStyles.playButton} color="accent" aria-label="edit" className={playButtonClass}>
+            <PlayArrowIcon style={overrideStyles.playArrowIcon} />
+          </IconButton>
+
         </Link>
-        <Button fab color="accent" aria-label="edit" className={classes.playButton + ' ' + playButtonHoverClass}>
-          <PlayArrowIcon />
+
+        <Button style={overrideStyles.addButton} fab color="accent" aria-label="edit" className={addButtonClass}
+          onMouseEnter={ () => this.setState({addHover:true, playHover:false})} onMouseLeave={ () => this.setState({addHover:false, playHover:true})}>
+          <AddIcon/>
         </Button>
       </div>
     );
